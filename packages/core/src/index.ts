38014c35430df915fc68e95ce61d40adfc6b6634
alberto <alyoung1991@gmail.com>
2023-03-05 12:07:42 -0800

    update

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -3,3 +3,8 @@ import { print } from '@typescript-library-template/utilities';
 export const corePrint = () => {
   print('core');
 };
+
+export const sayHi = () => {
+  console.log('Hiya from ts-lib-demo');
+  return 'Hi';
+};
--- packages/core/src/module/index.ts
+++ packages/core/src/module/index.ts
@@ -3,3 +3,8 @@ import { print } from '@typescript-library-template/utilities';
 export const modulePrint = () => {
   print('module');
 };
+
+export const sayHello = () => {
+  console.log('Hello from ts-lib-demo');
+  return 'Hello';
+};
